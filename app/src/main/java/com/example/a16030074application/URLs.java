91e9f28a68309668f2d0b04d2b8d538bd615b820
GalihDwiPutra <galihdp007@gmail.com>
2019-07-02 17:21:47 +0700

    test

---
+++ app/src/main/java/com/example/a16030074application/URLs.java
@@ -0,0 +1,10 @@
+package com.example.a16030074application;
+
+public class URLs {
+
+        private static final String ROOT_URL = "http://localhost/phpmyadmin/sql.php?db=android&table=users&pos=0";
+
+        public static final String URL_REGISTER = ROOT_URL + "signup";
+        public static final String URL_LOGIN= ROOT_URL + "login";
+
+    }
